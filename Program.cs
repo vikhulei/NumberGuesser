22baf9572364ee905411d9a059141e72a3e34f94
vikhulei <vikhulei@gmail.com>
2022-07-19 13:33:07 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while (true) {
+
+            Console.WriteLine("Hello. In this game you need to guess the number between 1 and 10. Please type your answer:");
+
+                int guess = 0;
+
+                var random = new Random();
+
+                int correctNumber = random.Next(1, 10);
+
+                while (guess != correctNumber)
+                {
+                    string input = Console.ReadLine();
+
+                    if (!int.TryParse(input, out guess))
+                    {
+                        ChangeConsoleColor(ConsoleColor.Red, "Enter an integer, please");
+                    } else
+                    {
+                        guess = Int32.Parse(input);
+
+                        if (guess != correctNumber)
+                        {
+                            ChangeConsoleColor(ConsoleColor.Red, "Not correct. Try again");
+                        }
+                    }
+
+                }
+
+                ChangeConsoleColor(ConsoleColor.Green, "Correct, well done!");
+
+                Console.WriteLine("Do you want to play again?");
+
+                string question = Console.ReadLine().ToUpper();
+
+                if (question == "Y")
+                {
+                    continue;
+                } else
+                {
+                    return;
+                }
+            }
+        }
+        static void ChangeConsoleColor(ConsoleColor col, string message)
+        {
+            Console.ForegroundColor = col;
+            Console.WriteLine(message);
+            Console.ResetColor ();
+        }
+    }
+}
